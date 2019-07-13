78dc383b4c03a766f5ca9f99c27fb5a0ba8e4c91
Sarah Harrs <sarahharrs@gmail.com>
2019-07-13 22:25:03 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School
+  attr_accessor :student :age :grade
+  attr_raeder :roster
+  roster = [ ]
+  def initialize (school)
+    @school = school
+  end
+  end
+  school= School.new ("Bayside High School")# code here
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.roster
